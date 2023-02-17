69c716861fa982b4de9e2467c228cd873fbfeaa7
aken <aken@kakao.com>
2023-02-17 19:21:17 +0900

    @SpringBootApplication conflict 해결

--- src/main/java/com/study/config/autoconfig/PropertySourcesPlaceholderConfig.java
+++ src/main/java/com/study/config/autoconfig/PropertySourcesPlaceholderConfig.java
@@ -1,10 +1,12 @@
 package com.study.config.autoconfig;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 public class PropertySourcesPlaceholderConfig {
   @Bean
+  @ConditionalOnMissingBean
   public PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer(){
     return new PropertySourcesPlaceholderConfigurer();
   }
